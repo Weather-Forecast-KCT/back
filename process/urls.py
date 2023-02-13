662eb0538913d76f4a8aa568b0910cf554feaea1
agilarasu <agilarasusaravanan@gmail.com>
2023-02-13 10:31:56 +0530

    included API function

--- process/urls.py
+++ process/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hello),]
+    path('to_react', views.to_react),#just to test if this function is working
+    path('',views.main),#this is the main body of the backend and this will be used as api
+    #the data will only be collected when the api is used somewhere
+    ]
--- process/views.py
+++ process/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import JsonResponse
+#call this function for sending realtime data
+def to_react(request,data):
+    return JsonResponse(data,safe=False)#change safe = True if the data is a dictionary
 
-def say_hello(request):
-    return HttpResponse("Hello World")
+def main(request):
+    return JsonResponse("Main is working",safe=False)
+
