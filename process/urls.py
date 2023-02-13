512997fec2f187afb898db9faf330df27ce594cc
agilarasu <agilarasusaravanan@gmail.com>
2023-02-13 12:15:00 +0530

    Can receive data
    Will be able to receive json through a MQTT Broker

--- process/urls.py
+++ process/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('to_react', views.to_react),#just to test if this function is working
-    path('',views.main),#this is the main body of the backend and this will be used as api
-    #the data will only be collected when the api is used somewhere
     ]
--- process/views.py
+++ process/views.py
@@ -1,8 +1,35 @@
-from django.http import JsonResponse
+import paho.mqtt.client as mqtt
+import json
+from django.http import JsonResponse , HttpResponse
+from colorama import Fore, Style # for good experience in command line
+unfiltered_data={}
 #call this function for sending realtime data
-def to_react(request,data):
-    return JsonResponse(data,safe=False)#change safe = True if the data is a dictionary
+def to_react(request):
+    return JsonResponse(unfiltered_data,safe=False)#change safe = True if the data is a dictionary
+#call this function whenever the message is recieved
+def on_message(client, userdata,message):
+    global unfiltered_data
+    unfiltered_data =message.payload
+    unfiltered_data=unfiltered_data.decode('utf-8')
+    #filter the unfiltered data and connect to databases in v0.1.1
+    #filtered_data = unfiltered_data - useless
+    print(Fore.GREEN+"Got a message in the topic : "+message.topic+Style.RESET_ALL)
+    print(Fore.YELLOW+unfiltered_data+Style.RESET_ALL)
+    
 
-def main(request):
-    return JsonResponse("Main is working",safe=False)
-
+#this part Subscribes to the MQTT Broker . This happens only one time one the first boot . If it can't connect to mqtt it will show error on console
+try:
+    client = mqtt.Client()
+    #client.on_message = on_message
+    client.username_pw_set("clan4", "clan4")
+    client.connect("192.168.29.79", 1884, 10)
+    client.subscribe("weather", 0)
+    print(Fore.GREEN+"Successfully subscribed to MQTT server"+Style.RESET_ALL)
+    client.loop_start()
+    client.on_message = on_message
+    
+    
+except:
+    print(Fore.RED+"Error connecting to MQTT Broker")
+    print("Please check the MQTT Broker and try again"+Style.RESET_ALL)
+    exit()
